a76d8b7b09e7648d32f16a3b93c3a4c7ec00d0ea
gpspelle <gpsunicamp016@gmail.com>
2020-09-22 01:03:01 +0200

    send

--- audio.py
+++ audio.py
@@ -7,19 +7,25 @@ from adjustText import adjust_text
 
 
 # Input:
-# l: list of values
+# d: dictionary containing central value of a list of values. The key of d is the average of the values that this key holds.
 # v: test value
 
 # Output
-# returns true if and only if there exists values on l that are close
-# to v. The close definition is based in a 10% distance.
-def close(l, v):
+# No output
+def close(d, v):
 
-    for e in l:
-        if 0.90 * e <= v and 1.10 * e >= v:
-            return True
+    already_belongs = False
+    for peak_average in d.keys():
+        if 0.90 * peak_average <= v and 1.10 * peak_average >= v: # check if element is close on this neighborhood
+            already_belongs = True
+            d[peak_average].append(v) # add new value to a list of neighbors
+            pop_val = d.pop(peak_average)
+            d[np.mean(pop_val)] = pop_val # recreate a new list on the dictionary with its updated value
+            break
 
-    return False
+    if not already_belongs:
+        d[v] = [v]
+
 
 # Input:
 # vector: list of values
@@ -88,15 +94,12 @@ for record in records: # one file at a time
 
     # This part is only to annotate the plot and make it easier to visualize
     texts = []
-    positions = []
+    positions = dict()
     for i in range(len(sorted_peak_values)):
         peak_position = sorted_peak_values[i][0]
 
         if xf[peak_position] > 0:
-
-            if not close(positions, xf[peak_position]):
-                positions.append(xf[peak_position])
-
+            close(positions, xf[peak_position])
             texts.append(plt.text(xf[peak_position], yplot[peak_position], "{:.2f}".format(xf[peak_position]), fontsize=8))
 
     # Plot
@@ -114,11 +117,10 @@ for record in records: # one file at a time
 
     # However, we found it was better to use the average of distance between the first three peaks
     # as the fundamental frequency
-    positions = positions[:3]
-    positions.sort()
+    positions = sorted(list(positions.keys()))[:3]
     fundamental_frequency = np.mean(np.diff(positions))
     freq = better_fit(list(strings.values()), fundamental_frequency)
 
     for key in strings.keys():
         if freq == strings[key]:
-            print(record, key, strings[key])
+            print(record, key, strings[key], fundamental_frequency)
